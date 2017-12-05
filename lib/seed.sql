b27a27e570d1951408f8d9798bdf68250f4e3550
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-05 17:01:11 +0000

    automatically backed up by learn

--- lib/seed.sql
+++ lib/seed.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-VALUES (1, "Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-VALUES (2, "Rowdy", 10, "M", "black", "intense", 1),
-VALUES (3, "Tabitha", 6, "F", "dark brown", "nice", 1),
-VALUES (4, "Sargeant Brown", 19, "M", "Green", "playful", 0),
-VALUES (5, "Melissa", 13, "F", "dark brown", "goofy", 1),
-VALUES (6, "Grinch", 2, "M", "Black", "moody", 1),
-VALUES (7, "Wendy", 6, "F", "Blue", "naive", 1),
-VALUES (8, NULL, 20, "M", "black", "aggressive", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2, "Rowdy", 10, "M", "black", "intense", 1),
+(3, "Tabitha", 6, "F", "dark brown", "nice", 1),
+(4, "Sargeant Brown", 19, "M", "Green", "playful", 0),
+(5, "Melissa", 13, "F", "dark brown", "goofy", 1),
+(6, "Grinch", 2, "M", "Black", "moody", 1),
+(7, "Wendy", 6, "F", "Blue", "naive", 1),
+(8, null, 20, "M", "black", "aggressive", 0);
