dad5691c3edea3968bbe9a797849e1bf7b4c2b4b
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-05 16:31:48 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES
+INSERT INTO bears VALUES
+INSERT INTO bears VALUES
+INSERT INTO bears VALUES
+INSERT INTO bears VALUES
+INSERT INTO bears VALUES
+INSERT INTO bears VALUES
